15e215c90aaed5faef7dead15dad238b54da7702
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2022-04-28 09:34:51 +0200

    Add function for loading data into BigQuery

---
+++ src/load.py
@@ -0,0 +1,27 @@
+from google.cloud import bigquery
+
+
+
+def load_to_bigquery(df, dataset_id, table_id):
+    '''Load a Pandas `DataFrame` to a Google `BigQuery table`
+
+    ## Parameters
+    - df: a local DataFrame
+    - dataset_id: ID of a Google BigQuery dataset
+    - table_id: ID of a Google BigQuery table
+    '''
+    client = bigquery.Client()
+
+    dataset = client.dataset(dataset_id)
+    
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        write_disposition='WRITE_APPEND',
+        source_format=bigquery.SourceFormat.CSV
+    )
+
+    load_job = client.load_table_from_dataframe(df, 
+        dataset.table(table_id), 
+        job_config=job_config, 
+        location='eu'
+    )  # BigQueryAPI request    
