57f889d49635e4e45ef3d6c56fcf0ae2598d9439
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2022-04-28 09:36:32 +0200

    Load cityDim data into Bigquery

---
+++ src/api_cities_to_bq.py
@@ -0,0 +1,8 @@
+from pandas import read_csv
+from config import API_CITIES
+from .load import load_to_bigquery
+
+
+# Act as enrichment data
+city_dim = read_csv(API_CITIES)
+load_to_bigquery(city_dim, 'forecasts', 'city')
