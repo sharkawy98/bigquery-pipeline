a21ece8a1d3c17f78d21571d0753db5d69b02e28
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2022-04-29 06:45:11 +0200

    Create custom logger

---
+++ src/helpers/log.py
@@ -0,0 +1,20 @@
+from sys import stdout
+from loguru import logger
+
+
+
+def create_logger():
+    '''Create custom logger'''
+    logger.remove()
+    logger.add(
+        stdout,
+        colorize=True,
+        catch=True,
+        format='<light-cyan>{time:DD-MM-YYYY HH:mm:ss}</light-cyan> | '
+        + '<light-green>{level}</light-green>: '
+        + '<light-white>{message}</light-white>',
+    )
+    return logger
+
+
+LOGGER = create_logger()
